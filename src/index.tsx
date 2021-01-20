5f448a8b341986211bdd790a7af26d9cb9450fc5
Daniel Große <daniel.grosse@staffbase.com>
2021-01-20 14:01:03 +0100

    setup minimal test case and bundle

--- src/index.tsx
+++ src/index.tsx
@@ -11,78 +11,48 @@
  * limitations under the License.
  */
 
-import React from "react";
-import ReactDOM from "react-dom";
+import { BlockFactory, BlockDefinition, ExternalBlockDefinition } from "widget-sdk";
 
-import { BlockElement, BlockFactory, BlockDefinition, ExternalBlockDefinition } from "widget-sdk";
-import { GrodaTestProps, GrodaTest } from "./groda-test";
-import { configurationSchema } from "./configuration-schema";
-import { author, version } from '../package.json'
+const factory: BlockFactory = Base => {
+    /**
+     *  <groda-test message="world!"></groda-test>
+     */
+    return class GrodaTestBlock extends Base {
 
-const factory: BlockFactory = ({
-  HTMLElement,
-}: typeof window = window): Function => {
-  /**
-   *  <groda-test message="world!"></groda-test>
-   */
-  return class GrodaTestBlock extends HTMLElement
-    implements BlockElement {
-    public constructor() {
-      super();
-    }
+        public constructor() {
+            super();
+        }
 
-    private get props(): GrodaTestProps {
-      return {
-        message: this.getAttribute("message") || "",
-        list: this.getAttribute("list") || "",
-        select: this.getAttribute("select") || "",
-      };
-    }
+        public renderBlock(container: HTMLElement): void {
+            const div = document.createElement('div')
+            div.innerText="test";
+            container.appendChild(div)
+        }
 
-    public connectedCallback(): void {
-      this.reactRender();
-    }
+        unmountBlock(container: HTMLElement): void {
+            container.innerHTML = '';
+        }
 
-    private reactRender(): void {
-      ReactDOM.render(<GrodaTest {...this.props} />, this);
-    }
-
-    public static get observedAttributes(): string[] {
-      return ["message", "list", "select"];
-    }
-
-    public attributeChangedCallback(): void {
-      this.reactRender();
-    }
-
-    public adoptedCallback(): void {
-      // noop
-    }
-
-    public disconnectedCallback(): void {
-      ReactDOM.unmountComponentAtNode(this);
-    }
-
-    public unmount(): void {
-      ReactDOM.unmountComponentAtNode(this);
-      this.classList.remove('mceNonEditable');
-      this.classList.remove('connected');
-    }
-  };
+        public renderBlockInEditor(container: HTMLElement): void {
+            const div = document.createElement('div')
+            div.innerText="editor";
+            container.appendChild(div)
+        }
+    };
 };
 
 const blockDefinition: BlockDefinition = {
-  name: "groda-test",
-  factory: factory,
-  attributes: ['message'],
-  blockLevel: 'block',
-  configurationSchema: configurationSchema,
+    name: "groda-test",
+    factory: factory,
+    attributes: [],
+    blockLevel: 'block',
+    configurationSchema: {},
 };
 
 const externalBlockDefinition: ExternalBlockDefinition = {
-  blockDefinition,
-  author,
-  version
+    blockDefinition,
+    author: "xx",
+    version: "1.9.9"
 };
 
 window.defineBlock(externalBlockDefinition);
